c0e20b46a429dd8098b3c29693c586edfdc054b9
Bernardo Duarte <41548476+BernardoSoD@users.noreply.github.com>
2022-05-16 22:39:25 -0300

    Add files via upload

--- src/midia.cpp
+++ src/midia.cpp
@@ -4,38 +4,130 @@
 #include "headers/musica.hpp"
 #include "headers/filme.hpp"
 
-#include "filme.cpp"
-#include "musica.cpp"
-
+#include <map>
 #include <vector>
 using namespace std;
 
 template<typename T>
-void printList(vector<T> objects, string str) {
+void printList(vector<T> objects, string midia) {
 	if (objects.size() == 0) {
-		cout << "\nNenhum(a) " << str << " para ser listado(a)\n";
+		cout << "\nNenhum(a) " << midia << " para ser listado(a)\n";
 		return;
 	}
 	cout << "___________________________________________\n"
-		<< "Lista de " << str << "s:\n\n"
+		<< "\tLista de " << midia << "s:\n\n"
 		<< "    Titulo - Ano de lancamento\n";
 	for (size_t i = 0; i < objects.size(); i++)
 		cout << "(" << i + 1 << ") " << objects[i].getTitulo() << " - " << objects[i].getData() % 10000 << "\n";
 	cout << "___________________________________________\n";
 }
-template<typename T>
-void listByTitulo(vector<T> objects, string str) {
-	sort(objects.begin(), objects.end(), [](T& obj1, T& obj2)
-		{ return obj1.getTitulo() < obj2.getTitulo(); });
-	printList<T>(objects, str);
+void printArtistasList(vector<string> artistas, string midia) {
+	if (artistas.size() == 0) {
+		cout << "\nNenhum artista do(a) " << midia << " para ser listado\n";
+		return;
+	}
+	cout << "___________________________________________\n"
+		 << "\tLista de artistas do(a) " << midia << "s: \n\n";
+	for (size_t i = 0; i < artistas.size(); i++)
+		cout << "(" << i + 1 << ") " << artistas[i] << "\n";
+	cout << "___________________________________________\n";
 }
 template<typename T>
-void listByData(vector<T> objects, int anoDeLancamento, string str) {
+void listArtistasByT(vector<T> objects, string midia) {
+	vector<string> artistas;
+	for (size_t i = 0; i < objects.size(); i++) {
+		vector<string> art = objects[i].getArtistas();
+		artistas.insert(artistas.end(), art.begin(), art.end());
+	}
+	sort(artistas.begin(), artistas.end());
+	artistas.erase(unique(artistas.begin(), artistas.end()), artistas.end());
+
+	printArtistasList(artistas, midia);
+}
+template<typename T>
+void listOrderByData(vector<T> objects, string midia, bool decrescente) {
+	if (decrescente)
+		sort(objects.begin(), objects.end(), [](T& obj1, T& obj2)
+			{ return obj1.getData() > obj2.getData(); });
+	else
+		sort(objects.begin(), objects.end(), [](T& obj1, T& obj2)
+			{ return obj1.getData() < obj2.getData(); });
+	printList<T>(objects, midia);
+}
+template<typename T>
+void listByTitulo(vector<T> objects, string midia) {
+	sort(objects.begin(), objects.end(), [](T& obj1, T& obj2)
+		{ return obj1.getTitulo() < obj2.getTitulo(); });
+	printList<T>(objects, midia);
+}
+template<typename T>
+void listByData(vector<T> objects, int anoDeLancamento, string midia) {
 	vector<T> filteredObjectsByYear;
 	copy_if(objects.begin(), objects.end(), back_inserter(filteredObjectsByYear), [&](T &obj)
 		{ return obj.getData() % 10000 == anoDeLancamento; });
 
-	listByTitulo<T>(filteredObjectsByYear, str);
+	listByTitulo<T>(filteredObjectsByYear, midia);
+}
+
+void countMusicasByGenero(vector<Musica> musicas)
+{
+	map<GeneroDeMusica, int> counter;
+	vector<GeneroDeMusica> generos;
+
+	vector<string> generosDeMusica;
+	generosDeMusica.push_back("Null_AudioGenre");
+	generosDeMusica.push_back("Rock");
+	generosDeMusica.push_back("Heavy_Metal");
+	generosDeMusica.push_back("Eletronica");
+	generosDeMusica.push_back("Blues");
+	generosDeMusica.push_back("Jazz");
+	generosDeMusica.push_back("Pop");
+	generosDeMusica.push_back("Samb");
+
+	for (size_t i = 0; i < musicas.size(); i++)
+	{
+		generos = musicas[i].getGeneroDeMusica();
+		for_each(begin(generos), end(generos), [&](GeneroDeMusica const& s)
+			{ ++counter[s]; });
+	}
+
+	cout << endl
+		<< "Contagem de gêneros musicais" << endl
+		<< endl;
+	for (auto const& pair : counter)
+	{
+		cout << generosDeMusica[pair.first] << ": " << pair.second << endl;
+	}
+}
+
+void countFilmesByGenero(vector<Filme> filmes)
+{
+	map<GenerosDeFilme, int> counter;
+	vector<GenerosDeFilme> generos;
+
+	vector<string> generoDeFilme;
+	generoDeFilme.push_back("Null_MovieGenre");
+	generoDeFilme.push_back("Terror");
+	generoDeFilme.push_back("Suspense");
+	generoDeFilme.push_back("Acao");
+	generoDeFilme.push_back("Drama");
+	generoDeFilme.push_back("Documentario");
+	generoDeFilme.push_back("Comedia");
+
+	for (size_t i = 0; i < filmes.size(); i++)
+	{
+		generos = filmes[i].getGeneroDeFilme();
+		for_each(begin(generos), end(generos), [&](GenerosDeFilme const& s)
+			{ ++counter[s]; });
+	}
+
+	cout << endl
+		<< "Contagem de gêneros de filmes" << endl
+		<< endl;
+	for (auto const& pair : counter)
+	{
+		cout << generoDeFilme[pair.first] << ": " << pair.second << endl;
+	}
 }
 
 void listAllByTitulo(vector<Filme> filmes, vector<Musica> musicas)
@@ -52,6 +144,6 @@ void listAllByData(vector<Filme> filmes, vector<Musica> musicas, int anoDeLancam
 
 void listAllArtistas(vector<Filme> filmes, vector<Musica> musicas)
 {
-  listArtistasByMusicas(musicas);
-  listArtistasByFilmes(filmes);
+  listArtistasByT<Musica>(musicas, "musica");
+  listArtistasByT<Filme>(filmes, "filme");
 }
